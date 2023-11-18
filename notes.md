64b91a7f16cfad94065aed07b67f165b3ee117bc
Christopher Mangan <chrisjm66@gmail.com>
2023-11-18 02:32:53 -0500

    Sidebar started and almost completed.

---
+++ notes.md
@@ -0,0 +1,3 @@
+1. learn hooks
+2. zoom in css imcreases img size
+3. 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,7 +7,9 @@ import {
     faHome as HomeIcon,
     faUser as UserIcon,
     faEnvelope as EnvelopeIcon } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
+const logoColor = "#4d4d4e"
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
@@ -16,15 +18,32 @@ const Sidebar = () => (
         </Link>
         <nav>
             <NavLink exact='true' activeClassName='active' className='home-link' to='/'>
-                <FontAwesomeIcon icon = { HomeIcon } color="#4d4d4e" />
+                <FontAwesomeIcon icon = { HomeIcon } color={ logoColor } />
             </NavLink>
-            <NavLink exact='true' activeClassName='active' className ='about-link' to='/'>
-                <FontAwesomeIcon icon = { UserIcon } color="#4d4d4e" />
+            <NavLink exact='true' activeClassName='active' className ='about-link' to='/about'>
+                <FontAwesomeIcon icon = { UserIcon } color={ logoColor } />
             </NavLink>
-            <NavLink exact='true' activeClassName='active' className='contact-link' to='/'>
-                <FontAwesomeIcon icon = { EnvelopeIcon } color="#4d4d4e" />
+            <NavLink exact='true' activeClassName='active' className='contact-link' to='/contact'>
+                <FontAwesomeIcon icon = { EnvelopeIcon } color={ logoColor } />
             </NavLink>
         </nav>
+
+        <ul>
+            
+            <li>
+                <a href="https://linkedin.com/manganchris" rel='noreferrer'>
+                    <FontAwesomeIcon icon={ faLinkedin } color={ logoColor } />
+                </a>
+            </li>
+
+            <li>
+                <a href="https://github.com/chrisjm66" rel='noreferrer'>
+                    <FontAwesomeIcon icon={ faGithub } color={ logoColor } />
+                </a>
+            </li>
+
+        </ul>
+
     </div>    
 )
 
